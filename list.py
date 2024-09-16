bde372051fb09b8c6c4c6535f8ad89a92e014d05
Kaliandrik <contatokaliandrik@gmail.com>
2024-09-16 08:48:06 -0300

    Adicionar lista de nomes

---
+++ list.py
@@ -0,0 +1,11 @@
+#lista de nomes
+
+listanomes = []
+
+listanomes.append(input("Digite o primeiro nome: "))
+listanomes.append(input("Digite o segundo nome: "))
+listanomes.append(input("Digite o terceiro nome: "))
+listanomes.append(input("Digite o quarto nome: "))
+
+#exibir lista
+print(" Sua lista de nomes é: ", listanomes)
